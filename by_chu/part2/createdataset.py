37d26bbf4e46d680518fd758fc5295d3f6aa0da6
chu1pc <raiyu70@gmail.com>
2025-08-20 11:03:50 +0900

    created dataset

--- by_chu/part2/createdataset.py
+++ by_chu/part2/createdataset.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("eliasmarcon/luna-16")
+path = kagglehub.dataset_download("avc0706/luna16")
 
 print("Path to dataset files:", path)
